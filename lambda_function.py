dfd9137b95bc26ef4f161dce9dbdf9227cf129ad
Guilherme Alves <guilhermealves@Guilhermes-MacBook-Pro.local>
2022-05-15 14:21:50 -0300

    first-commit

---
+++ lambda_function.py
@@ -0,0 +1,32 @@
+import json
+
+import psycopg2
+
+
+def connect():
+    return psycopg2.connect(host="host do banco", database="seu banco", user="", password="")
+
+
+def lambda_handler(event, context):
+    try:
+        print('Connecting')
+        id_aluno = event['id_aluno']
+        nome = event['nome']
+        con = connect()
+        cur = con.cursor()
+        print('Saving')
+        cur.execute('insert into aluno values(%s,%s)', (id_aluno, nome))
+        con.commit()
+        con.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+
+
+
+
+
+
+
+
